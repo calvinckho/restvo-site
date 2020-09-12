d616072bf95acfb400a7a7b0d4c9b4c3d05d4097
Calvin Ho <ch235@cornell.edu>
2020-09-11 22:31:00 -0700

    change express script to site.js

--- server/processRequest.js
+++ server/processRequest.js
@@ -26,8 +26,7 @@ module.exports = {
     // don't index non production URLs, but favor HTTPS
     res.locals.protocol = 'https';
     if (
-      req.hostname.indexOf('ionicframework.com') == -1 &&
-      req.hostname.indexOf('ionic-site.herokuapp.com') == -1
+      req.hostname.indexOf('restvo.com') == -1
     ) {
       res.setHeader('X-Robots-Tag', 'noindex, nofollow');
       res.locals.protocol = 'http';
